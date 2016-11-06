9f6e31339da826163fd0f6b52d13bf546efe06af
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-07 06:56:54 +0900

    Format response (But I don't know how to format

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/twitter"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -29,7 +30,7 @@ func init() {
 }
 
 func main() {
-	db.AutoMigrate(&User{})
+	db.AutoMigrate(&User{}, &Message{})
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
@@ -39,7 +40,8 @@ func main() {
 	r.HandleFunc("/auth/callback", sessionCreateHandler)
 	r.HandleFunc("/logout", logoutHandler)
 
-	r.HandleFunc("/api/v1/messages", messagesHandler)
+	r.HandleFunc("/api/v1/messages", messagesHandler).Methods("GET")
+	r.HandleFunc("/api/v1/messages", messageCreateHandler).Methods("POST")
 
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
@@ -133,15 +135,74 @@ func messagesHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	var user User
-	db.First(&user, session.Values["user_id"])
+	var currentUser User
+	db.First(&currentUser, session.Values["user_id"])
 
 	var messages []Message
-	db.Model(&user).Related(&messages)
+	db.Find(&messages)
+
+	// for response
+	type ResponseUser struct {
+		Id       uint   `json:"id"`
+		Provider string `json:"provider"`
+		Uid      string `json:"uid"`
+		Nickname string `json:"nickname"`
+		ImageUrl string `json:"imageUrl"`
+	}
+
+	type ResponseMessage struct {
+		Id     uint         `json:"id"`
+		Text   string       `json:"text"`
+		Author ResponseUser `json:"author"`
+	}
+
+	var responseMessages []ResponseMessage
+	for _, message := range messages {
+		var user User
+		db.First(&user, message.UserID)
+
+		responseMessage := &ResponseMessage{
+			Id:   message.ID,
+			Text: message.Text,
+			Author: *&ResponseUser{
+				Id:       user.ID,
+				Provider: user.Provider,
+				Uid:      user.Uid,
+				Nickname: user.Nickname,
+				ImageUrl: user.ImageUrl,
+			},
+		}
+
+		responseMessages = append(responseMessages, *responseMessage)
+	}
 
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode(messages)
+	json.NewEncoder(w).Encode(responseMessages)
+}
+
+func messageCreateHandler(w http.ResponseWriter, r *http.Request) {
+	authenticate(w, r)
+
+	session, err := store.Get(r, SessionName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var user User
+	db.First(&user, session.Values["user_id"])
+
+	var message Message
+
+	body, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(body, &message)
+	message.UserID = user.ID
+
+	db.Model(&user).Create(&message)
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(message)
 }
 
 // models
@@ -157,6 +218,6 @@ type User struct {
 
 type Message struct {
 	gorm.Model
-	Text   string
 	UserID uint
+	Text   string
 }
